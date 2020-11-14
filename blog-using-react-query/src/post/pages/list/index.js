774c2b628a3264d9f5bc741d7ebca800816a8cc6
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2020-11-14 16:17:18 -0300

    feat(rq): add fetch all posts

--- blog-using-react-query/src/post/pages/list/index.js
+++ blog-using-react-query/src/post/pages/list/index.js
@@ -1,10 +1,18 @@
 import { Link } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import axios from 'axios';
 
 import Layout from '../../components/layout';
 import CreatePost from '../../components/create';
 
 function List() {
-  const posts = [];
+  const { data: posts, isLoading: postsLoading } = useQuery(
+    'posts',
+    async () => {
+      const response = await axios.get('http://localhost:1337/posts');
+      return response.data;
+    },
+  );
 
   const postList = posts?.map((post) => {
     return (
@@ -18,7 +26,7 @@ function List() {
   });
 
   return (
-    <Layout sidebar={<CreatePost />}>
+    <Layout loading={postsLoading} sidebar={<CreatePost />}>
       {posts?.length === 0 ? 'No posts found' : postList}
     </Layout>
   );
