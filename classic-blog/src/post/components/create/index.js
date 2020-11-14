0673e71e5976cb1e54bbd9ede43ee09f9effdede
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2020-11-14 17:37:17 -0300

    fix(classic): add error handling to post creation

--- classic-blog/src/post/components/create/index.js
+++ classic-blog/src/post/components/create/index.js
@@ -4,32 +4,47 @@ import axios from 'axios';
 function Create({ onCreate }) {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
+  const [error, setError] = useState(null);
 
   const handleClick = async (e) => {
     e.preventDefault();
 
-    await axios.post('http://localhost:1337/posts', {
-      title,
-      description,
-    });
+    try {
+      await axios.post('http://localhost:1337/posts', {
+        title,
+        description,
+      });
 
-    onCreate && onCreate();
+      onCreate && onCreate();
+    } catch (err) {
+      setError(err);
+    }
   };
 
   return (
-    <form>
-      Title:
-      <br />
-      <input type="text" onChange={(e) => setTitle(e.target.value)} />
-      <br />
-      Description:
-      <br />
-      <input type="text" onChange={(e) => setDescription(e.target.value)} />
-      <br />
-      <button type="submit" onClick={handleClick}>
-        Create
-      </button>
-    </form>
+    <div>
+      {error && (
+        <>
+          {error.message}
+          <br />
+          <br />
+        </>
+      )}
+
+      <form>
+        Title:
+        <br />
+        <input type="text" onChange={(e) => setTitle(e.target.value)} />
+        <br />
+        Description:
+        <br />
+        <input type="text" onChange={(e) => setDescription(e.target.value)} />
+        <br />
+        <button type="submit" onClick={handleClick}>
+          Create
+        </button>
+      </form>
+    </div>
   );
 }
 
