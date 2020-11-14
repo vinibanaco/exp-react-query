38af3d7343eedb3efc4b29f9735296cff230312a
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2020-11-14 16:15:57 -0300

    chore(rq): add react-query cache provider

--- blog-using-react-query/src/App.js
+++ blog-using-react-query/src/App.js
@@ -4,25 +4,30 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
+import { QueryCache, ReactQueryCacheProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query-devtools';
 
 import ListPostsPage from './post/pages/list';
 import PostDetailsPage from './post/pages/details';
 
+const queryCache = new QueryCache();
+
 function App() {
   return (
     <>
-      <Router>
-        <Switch>
-          <Redirect exact from="/" to="/posts" />
-          <Route exact path="/posts">
-            <ListPostsPage />
-          </Route>
-          <Route exact path="/posts/:id">
-            <PostDetailsPage />
-          </Route>
-        </Switch>
-      </Router>
+      <ReactQueryCacheProvider queryCache={queryCache}>
+        <Router>
+          <Switch>
+            <Redirect exact from="/" to="/posts" />
+            <Route exact path="/posts">
+              <ListPostsPage />
+            </Route>
+            <Route exact path="/posts/:id">
+              <PostDetailsPage />
+            </Route>
+          </Switch>
+        </Router>
+      </ReactQueryCacheProvider>
 
       <ReactQueryDevtools />
     </>
