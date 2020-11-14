9a0c77813968ced723d37fa102d778dbcf187255
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2020-11-14 19:57:13 -0300

    feat(classic): add pagination to posts list
    Add pagination to posts list.
    
    - Prev button is disabled if page is 1
    - Next button is disabled if page reaches the maximum page

--- classic-blog/src/post/pages/list/index.js
+++ classic-blog/src/post/pages/list/index.js
@@ -5,40 +5,82 @@ import axios from 'axios';
 import Layout from '../../components/layout';
 import CreatePost from '../../components/create';
 
+const LIST_PAGE_SIZE = 5;
+
 function List() {
   const [posts, setPosts] = useState();
   const [postsLoading, setPostsLoading] = useState(true);
+  const [postsCount, setPostsCount] = useState(0);
+  const [postsCountLoading, setPostsCountLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+
+  const maxPage = Math.ceil(postsCount / LIST_PAGE_SIZE);
+
+  const prevPage = () => {
+    setPage((currentPage) => Math.max(currentPage - 1, 1));
+  };
+
+  const nextPage = () => {
+    setPage((currentPage) => Math.min(currentPage + 1, maxPage));
+  };
+
+  const fetchPostsCount = useCallback(async () => {
+    try {
+      const { data } = await axios.get('http://localhost:1337/posts/count');
+      setPostsCount(data);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setPostsCountLoading(false);
+    }
+  }, []);
 
   const fetchPosts = useCallback(async () => {
     try {
-      const { data } = await axios.get('http://localhost:1337/posts');
+      const { data } = await axios.get('http://localhost:1337/posts', {
+        params: {
+          _start: (page - 1) * LIST_PAGE_SIZE,
+          _limit: LIST_PAGE_SIZE,
+        },
+      });
       setPosts(data);
     } catch (err) {
       setError(err);
     } finally {
       setPostsLoading(false);
     }
-  }, []);
+  }, [page]);
 
   useEffect(() => {
-    fetchPosts();
-  }, [fetchPosts]);
+    (async function fetchAndCountPosts() {
+      await Promise.all([fetchPosts(), fetchPostsCount()]);
+    })();
+  }, [fetchPosts, fetchPostsCount]);
 
-  const postList = posts?.map((post) => {
-    return (
-      <div key={post.id}>
-        <br />
-        <p>
-          <Link to={`/posts/${post.id}`}>{post.title}</Link>
-        </p>
-      </div>
-    );
-  });
+  const postList = (
+    <>
+      {posts?.map((post) => (
+        <div key={post.id}>
+          <br />
+          <p>
+            <Link to={`/posts/${post.id}`}>{post.title}</Link>
+          </p>
+        </div>
+      ))}
+      <br />
+      <button type="button" onClick={prevPage} disabled={page === 1}>
+        Prev
+      </button>{' '}
+      <button type="button" onClick={nextPage} disabled={page === maxPage}>
+        Next
+      </button>
+    </>
+  );
 
   return (
     <Layout
-      loading={postsLoading}
+      loading={postsLoading || postsCountLoading}
       sidebar={<CreatePost onCreate={fetchPosts} />}
       error={error}
     >
