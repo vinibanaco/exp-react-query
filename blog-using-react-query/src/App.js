630880688099e66e45dc7c0aed15e80981784fb9
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2020-11-14 15:54:36 -0300

    chore(rq): implement ReactQueryDevtools in App

--- blog-using-react-query/src/App.js
+++ blog-using-react-query/src/App.js
@@ -4,23 +4,28 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
+import { ReactQueryDevtools } from 'react-query-devtools';
 
 import ListPostsPage from './post/pages/list';
 import PostDetailsPage from './post/pages/details';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Redirect exact from="/" to="/posts" />
-        <Route exact path="/posts">
-          <ListPostsPage />
-        </Route>
-        <Route exact path="/posts/:id">
-          <PostDetailsPage />
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Switch>
+          <Redirect exact from="/" to="/posts" />
+          <Route exact path="/posts">
+            <ListPostsPage />
+          </Route>
+          <Route exact path="/posts/:id">
+            <PostDetailsPage />
+          </Route>
+        </Switch>
+      </Router>
+
+      <ReactQueryDevtools />
+    </>
   );
 }
 
