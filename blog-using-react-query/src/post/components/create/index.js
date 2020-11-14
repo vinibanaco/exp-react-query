575191db2451cce0f03cd8aa21ad3d0b9bc9ce1a
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2020-11-14 16:23:06 -0300

    feat(rq): add posts creation

--- blog-using-react-query/src/post/components/create/index.js
+++ blog-using-react-query/src/post/components/create/index.js
@@ -1,15 +1,45 @@
+import { useState } from 'react';
+import { useMutation, useQueryCache } from 'react-query';
+import axios from 'axios';
+
 function Create() {
+  const cache = useQueryCache();
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const [createPost] = useMutation(
+    async () => {
+      await axios.post('http://localhost:1337/posts', {
+        title,
+        description,
+      });
+    },
+    {
+      onSuccess: () => {
+        cache.invalidateQueries('posts');
+      },
+    },
+  );
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    createPost();
+  };
+
   return (
     <form>
       Title:
       <br />
-      <input type="text" />
+      <input type="text" onChange={(e) => setTitle(e.target.value)} />
       <br />
       Description:
       <br />
-      <input type="text" />
+      <input type="text" onChange={(e) => setDescription(e.target.value)} />
       <br />
-      <button type="submit">Create</button>
+      <button type="submit" onClick={handleClick}>
+        Create
+      </button>
     </form>
   );
 }
