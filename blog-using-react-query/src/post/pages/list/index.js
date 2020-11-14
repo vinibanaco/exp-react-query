c4f28df5b0356b4d0737f706fd2e3693c1ac2290
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2020-11-14 20:10:50 -0300

    feat(rq): add pagination to posts list
    Add pagination to posts list.
    
    - Prev button is disabled if page is 1
    - Next button is disabled if page reaches the maximum page

--- blog-using-react-query/src/post/pages/list/index.js
+++ blog-using-react-query/src/post/pages/list/index.js
@@ -1,32 +1,75 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useQuery } from 'react-query';
+import { usePaginatedQuery, useQuery } from 'react-query';
 import axios from 'axios';
 
 import Layout from '../../components/layout';
 import CreatePost from '../../components/create';
 
-function List() {
-  const { data: posts, isLoading: postsLoading, error } = useQuery(
-    'posts',
-    async () => {
-      const response = await axios.get('http://localhost:1337/posts');
-      return response.data;
-    },
-  );
+const LIST_PAGE_SIZE = 5;
 
-  const postList = posts?.map((post) => {
-    return (
-      <div key={post.id}>
-        <br />
-        <p>
-          <Link to={`/posts/${post.id}`}>{post.title}</Link>
-        </p>
-      </div>
-    );
+function List() {
+  const [page, setPage] = useState(1);
+
+  const {
+    data: postsCount,
+    isLoading: postsCountLoading,
+    error: postsCountError,
+  } = useQuery('postsCount', async () => {
+    const response = await axios.get('http://localhost:1337/posts/count');
+    return response.data;
   });
 
+  const {
+    resolvedData: posts,
+    isLoading: postsLoading,
+    error: postsError,
+  } = usePaginatedQuery(['posts', page], async (key, page) => {
+    const response = await axios.get('http://localhost:1337/posts', {
+      params: {
+        _start: (page - 1) * LIST_PAGE_SIZE,
+        _limit: LIST_PAGE_SIZE,
+      },
+    });
+    return response.data;
+  });
+
+  const maxPage = Math.ceil(postsCount / LIST_PAGE_SIZE);
+
+  const prevPage = () => {
+    setPage((currentPage) => Math.max(currentPage - 1, 1));
+  };
+
+  const nextPage = () => {
+    setPage((currentPage) => Math.min(currentPage + 1, maxPage));
+  };
+
+  const postList = (
+    <>
+      {posts?.map((post) => (
+        <div key={post.id}>
+          <br />
+          <p>
+            <Link to={`/posts/${post.id}`}>{post.title}</Link>
+          </p>
+        </div>
+      ))}
+      <br />
+      <button type="button" onClick={prevPage} disabled={page === 1}>
+        Prev
+      </button>{' '}
+      <button type="button" onClick={nextPage} disabled={page === maxPage}>
+        Next
+      </button>
+    </>
+  );
+
   return (
-    <Layout loading={postsLoading} sidebar={<CreatePost />} error={error}>
+    <Layout
+      loading={postsLoading || postsCountLoading}
+      sidebar={<CreatePost />}
+      error={postsError || postsCountError}
+    >
       {posts?.length === 0 ? 'No posts found' : postList}
     </Layout>
   );
